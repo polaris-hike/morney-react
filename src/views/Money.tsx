8cfb3bf672b2ccc5d8503bab74cbb6e5646460c4
wuxuweilalala <2509165479@qq.com>
2020-06-09 21:46:10 +0800

    money 组件 tag样式

--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -3,8 +3,30 @@ import React from 'react';
 import styled from 'styled-components';
 
 const TagsSection = styled.section`
-
+  padding: 0 16px;
+  background-color: #fff;
+  > ol {
+    padding: 0 4px;
+    margin: 12px -12px;
+      > li {
+          display: inline-block;
+          border-radius: 18px;
+          background-color: #D9D9D9;
+          padding: 3px 18px;
+          font-size: 14px;
+          margin: 8px 12px;
+      }
+  }
+  > button {
+    background: none;
+    border: none;
+    padding: 2px 4px;
+    border-bottom: 1px solid #333;
+    color: #666;
+    margin-top: 8px;
+  }
 `;
+
 const NotesSection = styled.section`
 
 `;
