f3800594b58aee8474075c153842c0b0a9bf8833
wuxuweilalala <2509165479@qq.com>
2020-06-13 22:11:41 +0800

    创建 createId

--- src/views/Money/TagsSection.tsx
+++ src/views/Money/TagsSection.tsx
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
 import React from 'react';
 import {useTags} from '../useTags';
+import {createId} from '../../lib/createId';
 
 const Wrapper = styled.section`
 display: flex;
@@ -44,7 +45,7 @@ const TagsSection: React.FC<Props> = (props) => {
     const onAddTag = () => {
         const tagName = window.prompt('新标签的名称为');
         if (tagName) {
-            setTags([...tags, {id:tags.length,name:tagName}]);
+            setTags([...tags, {id:createId(),name:tagName}]);
         }
     };
     const onSelectedTag = (tagId: number) => {
