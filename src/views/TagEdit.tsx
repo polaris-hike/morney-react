ebd1e2636470c15d10e585649c106afc12f76a1e
wuxuweilalala <2509165479@qq.com>
2020-06-14 15:34:42 +0800

    tag样式

--- src/views/TagEdit.tsx
+++ src/views/TagEdit.tsx
@@ -4,6 +4,24 @@ import {useParams} from 'react-router-dom';
 import Layout from 'components/Layout';
 import Icon from 'components/Icon';
 import {Button} from 'components/Button';
+import {Input} from 'components/Input'
+import {Center} from 'components/Center'
+import {Space} from 'components/Space'
+import styled from 'styled-components';
+
+const Topbar = styled.header`
+  display:flex;
+  justify-content: space-between;
+  align-items: center;
+  line-height: 20px;
+  padding: 14px;
+  background:white;
+`;
+const InputWrapper = styled.div`
+  background:white;
+  padding: 0 16px;
+  margin-top: 8px;
+`;
 
 const TagEdit: React.FC = () => {
     const {tags} = useTags();
@@ -13,23 +31,20 @@ const TagEdit: React.FC = () => {
     return (
         <div>
             <Layout>
-                <header>
-                    <Icon name="left"></Icon>
+                <Topbar>
+                    <Icon name="left"/>
                     <span>编辑标签</span>
-                </header>
-                <div>
-                    <label>
-                        <span>标签名</span>
-                        <input
-                            type="text"
-                            placeholder="标签名"
-                        />
-                    </label>
-                </div>
-                <div>
+                    <span/>
+                </Topbar>
+                <InputWrapper>
+                    <Input label="标签名" type="text" placeholder="标签名" value={tag.name}/>
+                </InputWrapper>
+                <Center>
+                    <Space/>
+                    <Space/>
+                    <Space/>
                     <Button>删除标签</Button>
-                </div>
-                {tag.name}
+                </Center>
             </Layout>
         </div>
     );
